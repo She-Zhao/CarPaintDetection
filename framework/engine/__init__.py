8ed418059487298eb3b8f1e1274af58016050b59
Alive <12591338+she_zhao@user.noreply.gitee.com>
2025-05-24 21:47:48 +0800

    为engine中各个文件添加了__init__.py 包内部均采用相对导入（包括__init__.py) 外部接口调用采用绝对导入

---
+++ framework/engine/__init__.py
@@ -0,0 +1,5 @@
+# engine/__init__.py
+from . import preprocess, pmd, detect
+from . import main_api
+
+__all__ = ['preprocess', 'pmd', 'detect', 'main_api']
---
+++ framework/engine/preprocess/__init__.py
@@ -0,0 +1,3 @@
+from .preprocess_api import run_preprocess  # 从当前目录的 run.py 导入 run_preprocess
+
+__all__ = ["run_preprocess"]     # 可选：定义外部可导入的符号
